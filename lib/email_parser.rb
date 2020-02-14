28077e21677e864d6ebc18d71bbc979e718aa234
Erin Stansbury <ekstansbury@gmail.com>
2020-02-14 17:24:20 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,8 +1,10 @@
 class EmailAddressParser
   attr_accessor :email_addresses
+  @@all = []
 
-  def initialize
-    @email_addresses = []
+  def initialize(email_addresses)
+    @email_addresses = email_addresses
+    @@all << self
   end
 
   def parser
